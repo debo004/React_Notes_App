165cd3ae48fcce2f8534e905dacab0b30b3ccbe2
Debopriyo Goswami <126260211+debo004@users.noreply.github.com>
2023-07-27 17:08:24 +0530

    Update Notes.js
    Correction of Current Time

--- src/Components/Notes/Notes.js
+++ src/Components/Notes/Notes.js
@@ -16,7 +16,7 @@ export default function Notes(props) {
     let hrs = date.getHours();
     let amPm = hrs > 12 ? "PM" : "AM";
     hrs = hrs ? hrs : "12";
-    hrs = hrs > 12 ? hrs = 24-hrs : hrs;
+    hrs = hrs > 12 ? hrs = hrs - 12 : hrs;
 
     let min = date.getMinutes();
     min = min < 10 ? "0" + min : min;
